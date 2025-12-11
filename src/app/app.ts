8e4ca47a220e2709f98373a2d4662bdc338f3d70
Nis5680 <nishitshah5680@gmail.com>
2025-12-11 18:19:27 +0530

    2nd_change

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,10 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('dynamicstyle-app');
+  bgcolor="blue";
+  fontsize="50px";
+  headingSizeBig="80px";
+  headingSizeSmall="30px";
+
+  zoom=true;
 }
